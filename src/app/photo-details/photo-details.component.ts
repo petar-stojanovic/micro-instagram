a4a5f0dbf0bd79d88706109978704384efaf4358
petar-stojanovic <banakafala1a@gmail.com>
2024-12-19 14:51:48 +0100

    refactored photo details

--- src/app/photo-details/photo-details.component.ts
+++ src/app/photo-details/photo-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -7,12 +7,12 @@ import {ActivatedRoute} from '@angular/router';
   templateUrl: './photo-details.component.html',
   styleUrl: './photo-details.component.scss'
 })
-export class PhotoDetailsComponent {
+export class PhotoDetailsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {
-    this.route.params.subscribe((params) => {
-      console.log(params);
-    });
+  }
 
+  ngOnInit(): void {
+    console.log(this.route.snapshot.params)
   }
 }
