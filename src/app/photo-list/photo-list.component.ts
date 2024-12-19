21d2b967e47725ead1d12e14dd813db7a2c0b7b1
petar-stojanovic <banakafala1a@gmail.com>
2024-12-19 12:49:45 +0100

    added angular material

---
+++ src/app/photo-list/photo-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-list',
+  imports: [],
+  templateUrl: './photo-list.component.html',
+  styleUrl: './photo-list.component.scss'
+})
+export class PhotoListComponent {
+
+}
