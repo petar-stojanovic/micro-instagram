42aeeac2c12fbcd407460313b2d4fe5b97397633
petar-stojanovic <petar-stojanovic@github.com>
2024-12-20 12:49:54 +0100

    removed delay pipe

--- src/app/photo-details/photo-details.component.ts
+++ src/app/photo-details/photo-details.component.ts
@@ -5,24 +5,24 @@ import {MatCardModule} from '@angular/material/card';
 import {PhotoService} from '../services/photo.service';
 import {Photo} from '../interfaces/photo';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
-import {delay} from 'rxjs';
 
 @Component({
   selector: 'app-photo-details',
-  imports: [MatCardModule, MatButtonModule,MatProgressSpinner],
+  imports: [MatCardModule, MatButtonModule, MatProgressSpinner],
   templateUrl: './photo-details.component.html',
   styleUrl: './photo-details.component.scss'
 })
 export class PhotoDetailsComponent implements OnInit {
 
   photo: Photo | undefined;
+
   constructor(private route: ActivatedRoute, private photoService: PhotoService) {
   }
 
   ngOnInit(): void {
     console.log(this.route.snapshot.params)
     const id = +this.route.snapshot.params["id"];
-    this.photoService.getPhoto(id).pipe(delay(100000)).subscribe(photo => {
+    this.photoService.getPhoto(id).subscribe(photo => {
       console.log(photo);
       this.photo = photo;
     })
